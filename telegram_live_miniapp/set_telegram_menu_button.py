954dbe5651bc013ba21a8402eba28a469d6d2ffd
zott882-create <zott882@gmail.com>
2026-05-12 22:40:40 +0500

    Delete telegram_live_miniapp directory

--- telegram_live_miniapp/set_telegram_menu_button.py
+++
@@ -1,9 +0,0 @@
-import os, requests
-token = os.environ.get("BOT_TOKEN")
-url = os.environ.get("WEBAPP_URL")
-if not token or not url:
-    raise SystemExit("Set BOT_TOKEN and WEBAPP_URL")
-r = requests.post(f"https://api.telegram.org/bot{token}/setChatMenuButton", json={
-    "menu_button": {"type": "web_app", "text": "Live матчи", "web_app": {"url": url}}
-})
-print(r.status_code, r.text)
